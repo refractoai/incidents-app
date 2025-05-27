ea6b69213a216f9261b1db9302c87220b118d124
Refracto <refracto7@proton.me>
2025-05-27 20:00:07 +0000

    Urgency handler

---
+++ srv/src/main/java/customer/incident_management/handler/IncidentsStatusHandler.java
@@ -0,0 +1,23 @@
+package customer.incident_management.handler;
+
+import java.util.*;
+import com.sap.cds.services.handler.EventHandler;
+import com.sap.cds.services.handler.annotations.Before;
+import com.sap.cds.services.handler.annotations.ServiceName;
+import org.springframework.stereotype.Component;
+
+import cds.gen.incidentservice.Incidents;
+import cds.gen.incidentservice.Incidents_;
+
+@Component
+@ServiceName("IncidentService")
+public class IncidentsStatusHandler implements EventHandler {
+
+    @Before(event = CqnService.EVENT_CREATE, entity = Incidents_.CDS_NAME)
+    public void beforeCreate(Incidents incidents) {
+        String title = incidents.getTitle();
+        if (title != null && title.toLowerCase().contains("urgent")) {
+            incidents.setStatus("high");
+        }
+    }
+}
